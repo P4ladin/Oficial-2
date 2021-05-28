7f7e960dbb6faba2d03f4b462a1730997839e46a
Stan.exe <73562371+P4ladin@users.noreply.github.com>
2021-05-28 16:17:34 -0300

    Update Meus-livros.py
    ainda preso nessa pte, como incluir um bD sqlite nesse codigo aqui?

--- Quest-Aberta/Meus-livros.py
+++ Quest-Aberta/Meus-livros.py
@@ -1,6 +1,11 @@
+
+
+
+
+
 class livro :
-    def __init__(self, numero, nome, autor, editora):
-        self.numero = numero
+    def __init__(self, codigo, nome, autor, editora):
+        self.codigo = codigo
         self.nome = nome
         self.autor = autor
         self.editora = editora
@@ -17,8 +22,8 @@ class livro :
     def setEditora(self, editora):
            self.editora = editora
     
-    def getNumero(self):
-           return self.numero
+    def getCodigo(self):
+           return self.codigo
     
     def getNome(self):
            return self.nome
@@ -27,7 +32,47 @@ class livro :
            return self.autor
     
     def getEditora(self):
-           return self.editora              
+           return self.editora 
+       
+
+
+class Interface:
+    
+    alunos = []
+
+    def registra_Livro(self):
+        nome = input('Quais é o nome do livro?\n')
+        autor = input('Quem foi o autor da Obra?\n')
+        
+
+        self.livros.append(livro(nome, autor,))
+        print('livro adicionado!')
+
+    def procurar_Livros(self):
+        for i, livro in enumerate(self.livros):
+            print(i, livro.nome)
+
+    def mostrar_Lista(self): #n sei como fazer esse
+        
+
+
+    def loop(self):
+        while True:
+            cmd = input('\n1 - Listar livros\n2 - registrar livro\n3 - Mudar senha\n')
+            if cmd == '1':
+                self.procurar_Livros()
+            elif cmd == '2':
+                self.registraLivro()
+            elif cmd == '3':
+                self.mudar_senha()
+            else:
+                print('opção invalida. por favor, selecione APENAS os itens listados.')
+                continue
+
+
+if __name__ == '__main__':
+    interface = Interface()
+    interface.loop()             
     
 
 

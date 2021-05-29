5b9f35397a42824d06d83cc1b90e33ac098bda69
Stan.exe <73562371+P4ladin@users.noreply.github.com>
2021-05-29 06:29:13 -0300

    Update Meus-livros.py
    Almost complete, just a few more tests before the final build (I'm running on coffee rn)

--- Quest-Aberta/Meus-livros.py
+++ Quest-Aberta/Meus-livros.py
@@ -1,5 +1,4 @@
 import sqlite3
-from file import Function
 
 banco = sqlite3.connect('livros.db')
 
@@ -8,9 +7,8 @@ cursor = banco.cursor()
 
 class Interface:
     
-    livros = []
-
-    def registra_Livro(self):
+    def registra_Livro(self): #Registrar o livro no BD
+        
         cursor.execute("SELECT TABLE livros.db")
         nome = input('Quais é o nome do livro?\n')
         autor = input('Quem foi o autor da Obra?\n')
@@ -18,29 +16,50 @@ class Interface:
         lido = input('voce terminou a leitura dele? s/n\n')
         
         
-
         cursor.execute("INSERT INTO livros(nome, autor, editora, lido) VALUES("+nome+", "+autor+", "+editora+" "+lido+")")
         banco.commit()
         print('livro adicionado!')
 
-    def procurar_Livros(self):
+    def procurar_Livros(self): #Buscar livro existente no BD
+         
         print('Qual sera o metodo de pesquisa?\n')
-        input('Digite 1 para nome do livro\nDigite 2 para autor\nDigite 3 para cancelar')
+        opc = input('Digite 1 para nome do livro\nDigite 2 para autor\nDigite 3 para cancelar:\n')
+        if opc == 1:
+            resN = input('digite o nome do livro:\n')
+            resNF = cursor.execute("SELECT nome FROM livros WHERE "+resN+" == nome")
+            banco.commit()
+            print (resNF)
+            
+        elif opc == 2:
+            resAu = input('digite o nome do autor:\n')
+            resAF = cursor.execute("SELECT autor FROM livros WHERE "+resAu+" == autor")
+            banco.commit()
+            print(resAF)
+            
+        elif opc == 3:
+            return 0
+            
+            
+            
+            
+            
+
+    def mostrar_Lista(self): #Mostrar BD no terminal (não consegui, não lia o arquivo)
         
-
-    def mostrar_Lista(self): #n sei como fazer esse
+        print('o arquivo não pode ser lido na IDE. para mais informações, acesse o README.')
         
+        return 0
 
 
-    def loop(self):
+    def loop(self): #Est. repetição do menu principal
         while True:
             cmd = input('\n1 - Listar livros\n2 - registrar livro\n3 - Mudar senha\n')
             if cmd == '1':
                 self.procurar_Livros()
             elif cmd == '2':
-                self.registraLivro()
+                self.registra_Livro()
             elif cmd == '3':
-                self.mudar_senha()
+                self.mostrar_Lista()
             else:
                 print('opção invalida. por favor, selecione APENAS os itens listados.')
                 continue
